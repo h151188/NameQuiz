2c35b6d228d4666cbdc052507a167626fea0c9aa
h181206 <glenni-97@hotmail.com>
2019-02-07 11:12:12 +0100

    TEST

--- app/src/main/java/com/example/namequiz/MainActivity.kt
+++ app/src/main/java/com/example/namequiz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import android.widget.EditText
@@ -59,7 +60,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Exit the application
-    fun exitApplication() {
+    fun exitApplication(view: View) {
         finish();
         System.exit(0);
     }
