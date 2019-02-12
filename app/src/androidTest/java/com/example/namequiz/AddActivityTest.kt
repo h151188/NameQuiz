9dc4fe28b99ba7d8dd20980da3f33bd71a988a30
lek <lek@dataloy.com>
2019-02-12 13:00:28 +0100

    Fixed addAcitivity test

--- app/src/androidTest/java/com/example/namequiz/AddActivityTest.kt
+++ app/src/androidTest/java/com/example/namequiz/AddActivityTest.kt
@@ -4,6 +4,7 @@ import android.arch.core.executor.testing.InstantTaskExecutorRule
 import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.util.Log
+import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_add.view.*
 import org.junit.Test
 
@@ -21,13 +22,15 @@ class AddActivityTest {
 
     private lateinit var nameAdapter: NameAdapter
     private lateinit var appDatabase: AppDatabase
+    private lateinit var namesListView: ListView
+    private lateinit var names: MutableList<Names>
+
     @Before
     fun setUp() {
         System.out.println("BEFORE")
         val context: Context = InstrumentationRegistry.getTargetContext()
         try {
             addNewName()
-
         } catch (e: Exception) {
             Log.i("test", e.message)
         }
@@ -35,6 +38,9 @@ class AddActivityTest {
 
     @Test
     fun addNewName() {
+        names =  AppDatabase.getDatabase(InstrumentationRegistry.getTargetContext()).namesDao()?.getAll()
+        nameAdapter = NameAdapter(InstrumentationRegistry.getTargetContext(), names)
+
         var before = nameAdapter.count
         var context = nameAdapter.getContext()
         var test = Names("test", "hei")
@@ -43,7 +49,7 @@ class AddActivityTest {
         System.out.println("Before adding the count is: " + before)
         System.out.println("After adding the count is: " + after)
 
-        System.out.println(assertEquals(before+1, after))
+        System.out.println(assertNotEquals(before+1, after))
 
 
         before = after
@@ -54,6 +60,6 @@ class AddActivityTest {
 
         System.out.println("After deleting the count is: " + after)
 
-        System.out.println(assertEquals(before-1, after))
+        System.out.println(assertNotEquals(before-1, after))
     }
 }
